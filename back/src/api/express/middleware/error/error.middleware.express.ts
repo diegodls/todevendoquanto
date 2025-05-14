978d75f866ad0d51087edbb02669a8f5dd80bb1c
DiegoL <contato.diegoluisdasilva@gmail.com>
2025-05-14 16:27:32 -0300

    refac: add jwt errors treatment

--- back/src/api/express/middleware/error/error.middleware.express.ts
+++ back/src/api/express/middleware/error/error.middleware.express.ts
@@ -1,5 +1,11 @@
 import { NextFunction, Request, Response } from "express";
+import {
+  JsonWebTokenError,
+  NotBeforeError,
+  TokenExpiredError,
+} from "jsonwebtoken";
 import { ApiError } from "../../../../util/api.errors";
+import { AdminMiddlewareErrorsCodes } from "../../../../util/errors-codes/middleware.errors.codes/admin.authorization";
 
 export function errorMiddlewareExpress(
   error: Error & Partial<ApiError>,
@@ -9,11 +15,11 @@ export function errorMiddlewareExpress(
 ) {
   const status = error.status ?? 500;
 
-  const message = error.message ?? "Internal Server Error";
+  let message = error.message ?? "Internal Server Error";
 
-  const errors = error.errors;
+  const errors = error.errors || [];
 
-  const code = error.code;
+  let code = error.code;
 
   const timestamp = new Date().toLocaleString("pt-BR", {
     year: "numeric",
@@ -24,6 +30,21 @@ export function errorMiddlewareExpress(
     second: "2-digit",
     hour12: false,
   });
+
+  if (error instanceof TokenExpiredError) {
+    message = "Internal Server Error";
+    code = AdminMiddlewareErrorsCodes.E_0_MW_ADM_0005.code;
+  }
+
+  if (error instanceof JsonWebTokenError) {
+    message = "Internal Server Error";
+    code = AdminMiddlewareErrorsCodes.E_0_MW_ADM_0006.code;
+  }
+
+  if (error instanceof NotBeforeError) {
+    message = "Internal Server Error";
+    code = AdminMiddlewareErrorsCodes.E_0_MW_ADM_0007.code;
+  }
 
   res.status(status).json({ message, errors, code, timestamp });
 }
