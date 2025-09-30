1125bc64d753cc78d52e331353b5ed213083410a
DiegoL <contato.diegoluisdasilva@gmail.com>
2025-09-30 16:18:32 -0300

    fix: role is optional

--- back/src/infrastructure/validation/zod/schemas/user/UserProfileUpdateBodySchema.ts
+++ back/src/infrastructure/validation/zod/schemas/user/UserProfileUpdateBodySchema.ts
@@ -5,7 +5,8 @@ import { z } from "zod";
 const UserUpdateBodySchema = z.object({
   name: z.string().min(6).max(256).optional(),
   email: z.string().email().optional(),
-  role: z.nativeEnum(UserRole),
+  role: z.nativeEnum(UserRole).optional(),
+  is_active: z.boolean().optional(),
 }) satisfies z.ZodType<UserUpdateInputDTO>;
 
 export { UserUpdateBodySchema };
