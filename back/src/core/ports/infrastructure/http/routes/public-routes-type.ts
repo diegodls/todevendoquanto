3790e86b1c2257cb976c9b0760765299f8e6bd25
DiegoL <contato.diegoluisdasilva@gmail.com>
2025-12-02 13:07:04 -0300

    refactor: renaming to better separation

--- back/src/core/ports/infrastructure/http/routes/public-routes-type.ts
+++ back/src/core/ports/infrastructure/http/routes/public-routes-type.ts
@@ -1,6 +1,6 @@
 import { PublicRouteObjInterface } from "@/core/ports/infrastructure/http/routes/route-obj-interface";
 import { UserLoginController } from "@/infrastructure/http/express/controllers/auth/login-controller";
-import { CreateUserController } from "@/infrastructure/http/express/controllers/user/create-controller";
+import { CreateUserController } from "@/infrastructure/http/express/controllers/user/create-user-controller";
 
 export type PublicRoutesType = [
   PublicRouteObjInterface<CreateUserController>,
