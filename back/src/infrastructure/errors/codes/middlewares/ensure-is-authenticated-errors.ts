42b69eb1f08a3a74ccf0d33286efe00f99fc6326
diegodls <contato.diegoluisdasilva@gmail.com>
2025-12-26 19:24:44 -0300

    refactor: need to fully ensure, not just verify

--- back/src/infrastructure/errors/codes/middlewares/ensure-is-authenticated-errors.ts
+++ back/src/infrastructure/errors/codes/middlewares/ensure-is-authenticated-errors.ts
@@ -2,6 +2,7 @@ import { ErrorsCodeType } from "@/core/shared/errors/types/error-codes-type";
 
 interface ensureIsAuthenticatedErrors {
   E_0_MDW_AUTH_0001: ErrorsCodeType;
+  E_0_MDW_AUTH_0002: ErrorsCodeType;
 }
 
 export const ensureIsAuthenticatedErrors: ensureIsAuthenticatedErrors = {
@@ -9,6 +10,12 @@ export const ensureIsAuthenticatedErrors: ensureIsAuthenticatedErrors = {
     code: "E_0_MDW_AUTH_0001",
     details: "Auth header not send",
     actions: "Client must send a AuthHeader with a valid Bearer Token",
+    instance: "ensure-is-authenticated",
+  },
+  E_0_MDW_AUTH_0002: {
+    code: "E_0_MDW_AUTH_0002",
+    details: "Bearer Token not present or malformed",
+    actions: "Client must send a valid Bearer Token, with the required fields",
     instance: "ensure-is-authenticated",
   },
 };
