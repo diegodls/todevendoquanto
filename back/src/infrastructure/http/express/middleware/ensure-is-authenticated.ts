d5d49b6b0b97ae59721b4f988ea87b4428623e40
diegodls <contato.diegoluisdasilva@gmail.com>
2025-12-26 19:21:10 -0300

    refactor: need to fully ensure, not just verify

--- back/src/infrastructure/http/express/middleware/ensure-is-authenticated.ts
+++ back/src/infrastructure/http/express/middleware/ensure-is-authenticated.ts
@@ -1,6 +1,8 @@
 import { JwtVerifyTokenInterface } from "@/core/ports/infrastructure/protocols/jwt/jwt-verify-token-interface";
 import { UnauthorizedError } from "@/core/shared/errors/api-errors";
 import { ensureIsAuthenticatedErrors } from "@/infrastructure/errors/codes/middlewares/ensure-is-authenticated-errors";
+import { JwtPayloadInterfaceSchema } from "@/infrastructure/validation/zod/schemas/jwt/jwt-user-payload-schema";
+import { schemaParser } from "@/infrastructure/validation/zod/validation/schema-parser";
 import { NextFunction, Request, Response } from "express";
 
 export const ensureIsAuthenticated = (jwtAuth: JwtVerifyTokenInterface) => {
@@ -19,7 +21,13 @@ export const ensureIsAuthenticated = (jwtAuth: JwtVerifyTokenInterface) => {
 
     const decoded = await jwtAuth.execute<JwtPayloadInterface>(token);
 
-    request.user = decoded;
+    const parsedUser = schemaParser(
+      decoded,
+      JwtPayloadInterfaceSchema,
+      ensureIsAuthenticatedErrors.E_0_MDW_AUTH_0002.code
+    );
+
+    request.user = parsedUser;
 
     next();
   };
