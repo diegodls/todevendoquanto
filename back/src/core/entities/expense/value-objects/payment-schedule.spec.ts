298ace45755077f27259b8569b7734a8873d73de
diegodls <contato.diegoluisdasilva@gmail.com>
2026-01-22 20:31:51 -0300

    test: setting up test

---
+++ back/src/core/entities/expense/value-objects/payment-schedule.spec.ts
@@ -0,0 +1,11 @@
+import { describe, expect, it } from "vitest";
+
+describe("PaymentSchedule", () => {
+  describe("create", () => {
+    it("should do nothing, base test", () => {
+      const expected = "value";
+      const actual = "value";
+      expect(actual).toBe(expected);
+    });
+  });
+});
